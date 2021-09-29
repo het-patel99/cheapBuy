03a38bb2f62639683172c579bc74bab4a6012bd3
hvudeshi <hardikudeshi6800@gmail.com>
2021-09-29 10:51:29 -0400

    Added Web Scrapper for amazon

---
+++ web_scrappers/fetch_description_amazon.py
@@ -0,0 +1,11 @@
+def description_from_url_amazon(link):
+	link = link.strip('https://www.amazon.com/')
+	description = ''
+	for ch in link:
+		if ch!='/':
+			description+=ch
+		else:
+			break
+
+	description = description.replace('-', ' ')
+	return description
