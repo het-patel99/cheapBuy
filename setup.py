07dfb7356a74a18a2670d2a54f42f1d24c481c4a
Het Patel <phet0199@gmail.com>
2021-09-30 19:18:24 -0400

    added github workflow and docs

--- setup.py
+++ setup.py
@@ -2,7 +2,6 @@ from setuptools import setup
 
 setup(name='cheapBuy',
       version='1.0',
-      name='cheapBuy',
       description='cheapBuy Extension provides you ease to buy any product through your favourite website like Amazon, Walmart, Ebay, Bjs, Costco, etc, by providing prices of the same product from all different websites to extension.',
       author='Hardik, Het, Vineet, Saloni, Kalgee',
       author_email='hetpatel0199@gmail.com',
