4c9088746901b2af2c4ca49e69fd384a7cb9ea53
KALGEE ANAND KOTAK <43135408+kalgeekotak99@users.noreply.github.com>
2021-09-30 17:26:35 -0400

    Added test case 2

--- test/test.py
+++ test/test.py
@@ -3,20 +3,11 @@ import sys
 sys.path.append(os.path.abspath('../../'))
 from cheapBuy.test.test_web_scrapper_ebay import *
 from cheapBuy.test.test_web_scrapper import *
-from cheapBuy.test.test_server_api import *
+from cheapBuy.test.test_web_scrapper_amazon import *
 
 def test_answer():
    # test_web_scrapper.py
    test_get_driver()
-
-   # test_server_api.py
-   test_post_result()
-   test_post_result_status()
-   test_post_result_response_ebay()
-   test_post_result_response_bjs()
-   test_post_result_response_amazon()
-   test_post_result_response_costco()
-   test_post_result_response_walmart()
 
    # test_web_scrapper_ebay.py
    test_get_url_ebay_1()
@@ -25,3 +16,11 @@ def test_answer():
    test_extract_item_ebay_result_len()
    test_extract_item_ebay_result_site()
    test_extract_item_ebay_result_url()
+
+   # test_web_scrapper_amazon.py
+   test_get_url_amazon_1()
+   test_get_url_amazon_2()
+   test_scrap_amazon()
+   test_extract_item_amazon_result_len()
+   test_extract_item_amazon_result_site()
+   test_extract_item_amazon_result_url()
