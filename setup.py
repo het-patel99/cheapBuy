bfcfeef1bbb88c1358983c4fbcb7e3909d4d4cf9
Het Patel <hetpatel0199@gmail.com>
2021-09-30 15:38:04 -0400

    Updates setup.py

--- setup.py
+++ setup.py
@@ -8,5 +8,38 @@ setup(name='cheapBuy',
       author_email='hetpatel0199@gmail.com',
       url='https://github.com/het-patel99/cheapBuy.git',
       packages=['cheapBuy'],
-      license='MIT'
+      long_description="""\
+            Hands on for the standard github repo files.
+            .gitignore
+            .travis.yml
+            CITATION.md : fill on once you've got your ZENODO DOI going
+            CODE-OF-CONDUCT.md
+            CONTRIBUTING.md
+            LICENSE.md
+            README.md
+            setup.py
+            requirements.txt
+            data/
+              README.md
+            test/
+              README.md
+            code/
+              __init__.py
+        """,
+      classifiers=[
+            "License :: MIT License",
+            "Programming Language :: Python",
+            "Development Status :: Initial",
+            "Intended Audience :: Developers",
+            "Topic :: Software Engineering",
+        ],
+      keywords='python requirements license gitignore',
+      license='MIT',
+      install_requires=[
+            'BeautifulSoup',
+            'pytest',
+            'Flask',
+            'Flask-Cors',
+            'webdriver_manager'
+        ],
      )
