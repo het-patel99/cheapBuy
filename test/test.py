2428f35978f7cb45067e78d9450e2c8656b40f01
hvudeshi <hardikudeshi6800@gmail.com>
2021-09-30 15:50:44 -0400

    Update test case 1

--- test/test.py
+++ test/test.py
@@ -1,4 +1,7 @@
-from test_web_scrapper_ebay import *
+import os
+import sys
+sys.path.append(os.path.abspath('../../'))
+from cheapBuy.test.test_web_scrapper_ebay import *
 
 def test_answer():
 	test_get_url_ebay_1()
