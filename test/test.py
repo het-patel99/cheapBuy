9e21e0e94ada4db2641d4c0fa85a85dfbb73976b
KALGEE ANAND KOTAK <43135408+kalgeekotak99@users.noreply.github.com>
2021-09-30 17:01:36 -0400

    Add test case server_api

--- test/test.py
+++ test/test.py
@@ -3,11 +3,21 @@ import sys
 sys.path.append(os.path.abspath('../../'))
 from cheapBuy.test.test_web_scrapper_ebay import *
 from cheapBuy.test.test_web_scrapper import *
+from cheapBuy.test.test_server_api import *
 
 def test_answer():
    # test_web_scrapper.py
    test_get_driver()
-   
+
+   # test_server_api.py
+   test_post_result()
+   test_post_result_status()
+   test_post_result_response_ebay()
+   test_post_result_response_bjs()
+   test_post_result_response_amazon()
+   test_post_result_response_costco()
+   test_post_result_response_walmart()
+
    # test_web_scrapper_ebay.py
    test_get_url_ebay_1()
    test_get_url_ebay_2()
