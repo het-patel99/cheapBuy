71c38e4bd00c9d015f1f5d93797eec9c3e74ef9e
sal0ni <37000199+sal0ni@users.noreply.github.com>
2021-09-30 20:42:57 -0400

    Update test5.py

--- test/test5.py
+++ test/test5.py
@@ -6,8 +6,9 @@ from cheapBuy.test.test_web_scrapper_scrap_amazon import *
 
 
 def test_answer():
-	test_scrapper_walmart_result()
-	test_scrapper_walmart_result_len()
+    test_scrapper_walmart_result()
+    test_scrapper_walmart_result_len()
     test_scrapper_amazon_result()
     test_scrapper_amazon_result_len()
     test_scrapper_amazon_result_description_count()
+    
