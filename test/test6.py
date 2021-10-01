535bd19ae6e907820710a7df101ca95329d825f8
sal0ni <37000199+sal0ni@users.noreply.github.com>
2021-09-30 21:22:27 -0400

    Update test6.py

--- test/test6.py
+++ test/test6.py
@@ -6,3 +6,8 @@ from cheapBuy.test.test_web_scrapper_scrap_bjs import *
 
 def test_answer():
     test_scrapper_bjs_result()
+    test_scrapper_bjs_result_len()
+    test_scrapper_bjs_result_description_count()
+    test_scrapper_bjs_result_site()
+    test_scrapper_bjs_result_url_ebay()
+    test_scrapper_bjs_result_url_amazon()
--- test/test_web_scrapper_scrap_bjs.py
+++ test/test_web_scrapper_scrap_bjs.py
@@ -4,30 +4,25 @@ sys.path.append(os.path.abspath('../../'))
 from cheapBuy.code.web_scrappers.web_scrapper import scrapper
 
 def test_scrapper_bjs_result():
-    result = scrapper("https://www.bjs.com/product/apple-ipad-pro-11-3rd-generation-256gb-wi-fi---space-gray/3000000000001510763")
+    result = scrapper("https://www.bjs.com/product/brita-pour-through-pitcher-replacement-filter-10-pk/23578")
     assert result is not None
 
 def test_scrapper_bjs_result_len():
-    result = scrapper(
-        "https://www.bjs.com/product/tide-ultra-concentrated-liquid-laundry-detergent-208-fl-oz/3000000000002203255")
+    result = scrapper("https://www.bjs.com/product/brita-pour-through-pitcher-replacement-filter-10-pk/23578")
     assert len(result) == 4
 
 def test_scrapper_bjs_result_description_count():
-    result = scrapper(
-        "https://www.bjs.com/product/berkley-jensen-ultra-strong-bath-tissue-24-ct/3000000000001957283")
+    result = scrapper("https://www.bjs.com/product/brita-pour-through-pitcher-replacement-filter-10-pk/23578")
     assert len(result["description"]) == 2
 
 def test_scrapper_bjs_result_site():
-    result = scrapper(
-        "https://www.bjs.com/product/samsung-55-q6da-qled-4k-smart-tv---qn55q6daafxza-with-your-choice-subscription-and-3-year-warranty/3000000000003018763")
+    result = scrapper("https://www.bjs.com/product/brita-pour-through-pitcher-replacement-filter-10-pk/23578")
     assert result["site"] == ["amazon","ebay"]
 
 def test_scrapper_bjs_result_url_ebay():
-    result = scrapper(
-        "https://www.bjs.com/product/huggies-little-snugglers-baby-diapers/3000000000002993748")
+    result = scrapper("https://www.bjs.com/product/brita-pour-through-pitcher-replacement-filter-10-pk/23578")
     assert result["url"][1].find("ebay") != -1
 
 def test_scrapper_bjs_result_url_amazon():
-    result = scrapper(
-        "https://www.bjs.com/product/microsoft-surface-laptop-3-intel-core-i5-processor-8gb-memory-256gb-ssd/3000000000003180287")
+    result = scrapper("https://www.bjs.com/product/brita-pour-through-pitcher-replacement-filter-10-pk/23578")
     assert result["url"][0].find("amazon") != -1
