7c0beba28bbc7f32f7545f7f76dac23a274f5ef2
sal0ni <37000199+sal0ni@users.noreply.github.com>
2021-09-30 20:48:50 -0400

    Add test_web_scrapper_scrap_ebay.py

--- test/test5.py
+++ test/test5.py
@@ -3,6 +3,7 @@ import sys
 sys.path.append(os.path.abspath('../../'))
 from cheapBuy.test.test_web_scrapper_scrap_walmart import *
 from cheapBuy.test.test_web_scrapper_scrap_amazon import *
+from cheapBuy.test.test_web_scrapper_scrap_ebay import *
 
 
 def test_answer():
@@ -11,4 +12,11 @@ def test_answer():
     test_scrapper_amazon_result()
     test_scrapper_amazon_result_len()
     test_scrapper_amazon_result_description_count()
+    test_scrapper_amazon_result_site_ebay()
+    test_scrapper_amazon_result_url_ebay()
+    test_scrapper_ebay_result()
+    test_scrapper_ebay_result_len()
+    test_scrapper_ebay_result_description_count()
+    test_scrapper_ebay_result_site_amazon()
+    test_scrapper_ebay_result_url_amazon()
     
