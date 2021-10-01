c64d5f88626fd5df7be868ee86fbbe6e371a9fb8
sal0ni <37000199+sal0ni@users.noreply.github.com>
2021-09-30 20:22:17 -0400

    Update test_web_scrapper_scrap_walmart.py

--- test/test_web_scrapper_scrap_walmart.py
+++ test/test_web_scrapper_scrap_walmart.py
@@ -5,30 +5,30 @@ from cheapBuy.code.web_scrappers.web_scrapper import scrapper
 
 
 def test_scrapper_walmart_result():
-    result = scrapper("https://www.walmart.com/ip/PUMA-SAFETY-SHOES-642865-Athltc-Style-Wrk-Shoes-9-1-2C-Gry-Pnk-PR/113851508")
+    result = scrapper("https://www.walmart.com/ip/Brita-Standard-Water-Filter-Replacement-4-Count/16513272")
     assert result is not None
 
 def test_scrapper_walmart_result_len():
     result = scrapper(
-        "https://www.walmart.com/ip/PUMA-SAFETY-SHOES-642865-Athltc-Style-Wrk-Shoes-9-1-2C-Gry-Pnk-PR/113851508")
+        "https://www.walmart.com/ip/Brita-Standard-Water-Filter-Replacement-4-Count/16513272")
     assert len(result) == 4
 
 def test_scrapper_walmart_result_description_count():
     result = scrapper(
-        "https://www.walmart.com/ip/PUMA-SAFETY-SHOES-642865-Athltc-Style-Wrk-Shoes-9-1-2C-Gry-Pnk-PR/113851508")
+        "https://www.walmart.com/ip/Brita-Standard-Water-Filter-Replacement-4-Count/16513272")
     assert len(result["description"]) == 2
 
 def test_scrapper_walmart_result_site():
     result = scrapper(
-        "https://www.walmart.com/ip/PUMA-SAFETY-SHOES-642865-Athltc-Style-Wrk-Shoes-9-1-2C-Gry-Pnk-PR/113851508")
+        "https://www.walmart.com/ip/Brita-Standard-Water-Filter-Replacement-4-Count/16513272")
     assert result["site"] == ["amazon","ebay"]
 
 def test_scrapper_walmart_result_url_ebay():
     result = scrapper(
-        "https://www.walmart.com/ip/PUMA-SAFETY-SHOES-642865-Athltc-Style-Wrk-Shoes-9-1-2C-Gry-Pnk-PR/113851508")
+        "https://www.walmart.com/ip/Brita-Standard-Water-Filter-Replacement-4-Count/16513272")
     assert result["url"][1].find("ebay") != -1
 
 def test_scrapper_walmart_result_url_amazon():
     result = scrapper(
-        "https://www.walmart.com/ip/PUMA-SAFETY-SHOES-642865-Athltc-Style-Wrk-Shoes-9-1-2C-Gry-Pnk-PR/113851508")
+        "https://www.walmart.com/ip/Brita-Standard-Water-Filter-Replacement-4-Count/16513272")
     assert result["url"][0].find("amazon") != -1
