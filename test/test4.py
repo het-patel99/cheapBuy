99630e30433a9505ddbc11318531515b2c8f71b3
Vineet2311 <chhedavineet2311@gmail.com>
2021-09-30 20:55:15 -0400

    Test cases for fetch_description

---
+++ test/test4.py
@@ -0,0 +1,8 @@
+import os
+import sys
+sys.path.append(os.path.abspath('../../'))
+from cheapBuy.test.test_fetch_description_walmart import *
+
+def test_answer():
+   test_fetch_description_walmart1()
+   test_fetch_description_walmart2()
