708ddf41eec8b6366dc0bf2c9dc6d848a569b55d
sal0ni <37000199+sal0ni@users.noreply.github.com>
2021-09-30 21:15:28 -0400

    updated test6.py

--- test/test6.py
+++ test/test6.py
@@ -1,12 +1,8 @@
 import os
 import sys
 sys.path.append(os.path.abspath('../../'))
-from cheapBuy.test.test_web_scrapper_scrap_ebay import *
+from cheapBuy.test.test_web_scrapper_scrap_bjs import *
 
 
 def test_answer():
-    test_scrapper_ebay_result()
-    test_scrapper_ebay_result_len()
-    test_scrapper_ebay_result_description_count()
-    test_scrapper_ebay_result_site_amazon()
-    test_scrapper_ebay_result_url_amazon()
+    test_scrapper_bjs_result()
