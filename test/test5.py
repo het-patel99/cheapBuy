38bba84bf023f452665709ca5236f880e6c0f277
sal0ni <37000199+sal0ni@users.noreply.github.com>
2021-09-30 20:35:34 -0400

    Add test_web_scrapper_scrap_amazon.py

--- test/test5.py
+++ test/test5.py
@@ -2,8 +2,13 @@ import os
 import sys
 sys.path.append(os.path.abspath('../../'))
 from cheapBuy.test.test_web_scrapper_scrap_walmart import *
+from cheapBuy.test.test_web_scrapper_scrap_amazon import *
 
 
 def test_answer():
 	test_scrapper_walmart_result()
 	test_scrapper_walmart_result_len()
+    
+    test_scrapper_amazon_result()
+    test_scrapper_amazon_result_len()
+    test_scrapper_amazon_result_description_count()
