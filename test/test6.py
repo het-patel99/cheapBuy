755c1b2915330bb9505320aff3c169c69087f8f0
sal0ni <37000199+sal0ni@users.noreply.github.com>
2021-09-30 21:33:24 -0400

    Updated

--- test/test6.py
+++ test/test6.py
@@ -2,12 +2,12 @@ import os
 import sys
 sys.path.append(os.path.abspath('../../'))
 from cheapBuy.test.test_web_scrapper_scrap_bjs import *
+from cheapBuy.test.test_web_scrapper_scrap_costco import *
 
 
 def test_answer():
     test_scrapper_bjs_result()
     test_scrapper_bjs_result_len()
-    test_scrapper_bjs_result_description_count()
-    test_scrapper_bjs_result_site()
-    test_scrapper_bjs_result_url_ebay()
-    test_scrapper_bjs_result_url_amazon()
+    test_scrapper_costco_result()
+    test_scrapper_costco_result_len()
+    test_scrapper_costco_result_description_count()
--- test/test_web_scrapper_scrap_bjs.py
+++ test/test_web_scrapper_scrap_bjs.py
@@ -10,19 +10,3 @@ def test_scrapper_bjs_result():
 def test_scrapper_bjs_result_len():
     result = scrapper("https://www.bjs.com/product/brita-pour-through-pitcher-replacement-filter-10-pk/23578")
     assert len(result) == 4
-
-def test_scrapper_bjs_result_description_count():
-    result = scrapper("https://www.bjs.com/product/brita-pour-through-pitcher-replacement-filter-10-pk/23578")
-    assert len(result["description"]) == 2
-
-def test_scrapper_bjs_result_site():
-    result = scrapper("https://www.bjs.com/product/brita-pour-through-pitcher-replacement-filter-10-pk/23578")
-    assert result["site"] == ["amazon","ebay"]
-
-def test_scrapper_bjs_result_url_ebay():
-    result = scrapper("https://www.bjs.com/product/brita-pour-through-pitcher-replacement-filter-10-pk/23578")
-    assert result["url"][1].find("ebay") != -1
-
-def test_scrapper_bjs_result_url_amazon():
-    result = scrapper("https://www.bjs.com/product/brita-pour-through-pitcher-replacement-filter-10-pk/23578")
-    assert result["url"][0].find("amazon") != -1
