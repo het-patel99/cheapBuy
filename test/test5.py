73e0cf28b8e04575be2d2533e839d6efab434bc8
sal0ni <37000199+sal0ni@users.noreply.github.com>
2021-09-30 20:30:52 -0400

    update test_web_scrapper_scrap_walmart.py

--- test/test5.py
+++ test/test5.py
@@ -7,7 +7,3 @@ from cheapBuy.test.test_web_scrapper_scrap_walmart import *
 def test_answer():
 	test_scrapper_walmart_result()
 	test_scrapper_walmart_result_len()
-	test_scrapper_walmart_result_description_count()
-	test_scrapper_walmart_result_site()
-	test_scrapper_walmart_result_url_ebay()
-	test_scrapper_walmart_result_url_amazon()
--- test/test_web_scrapper_scrap_walmart.py
+++ test/test_web_scrapper_scrap_walmart.py
@@ -12,23 +12,3 @@ def test_scrapper_walmart_result_len():
     result = scrapper(
         "https://www.walmart.com/ip/Brita-Longlast-Water-Filter-Replacement-Reduces-Lead-2-Count/128876038")
     assert len(result) == 4
-
-def test_scrapper_walmart_result_description_count():
-    result = scrapper(
-        "https://www.walmart.com/ip/Brita-Longlast-Water-Filter-Replacement-Reduces-Lead-2-Count/128876038")
-    assert len(result["description"]) == 2
-
-def test_scrapper_walmart_result_site():
-    result = scrapper(
-        "https://www.walmart.com/ip/Brita-Longlast-Water-Filter-Replacement-Reduces-Lead-2-Count/128876038")
-    assert result["site"] == ["amazon","ebay"]
-
-def test_scrapper_walmart_result_url_ebay():
-    result = scrapper(
-        "https://www.walmart.com/ip/Brita-Longlast-Water-Filter-Replacement-Reduces-Lead-2-Count/128876038")
-    assert result["url"][1].find("ebay") != -1
-
-def test_scrapper_walmart_result_url_amazon():
-    result = scrapper(
-        "https://www.walmart.com/ip/Brita-Longlast-Water-Filter-Replacement-Reduces-Lead-2-Count/128876038")
-    assert result["url"][0].find("amazon") != -1
