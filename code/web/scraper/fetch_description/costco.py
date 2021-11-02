c8d62607d48a35abbb078c960aa576d424ebc11f
aakriti0fnu <aakriti.cse@gmail.com>
2021-11-01 20:05:10 -0400

    update costco scraper.

--- code/web/scraper/fetch_description/costco.py
+++ code/web/scraper/fetch_description/costco.py
@@ -1,16 +1,23 @@
 def description_from_url_costco(link):
-	description = ''
-	try:
-		remove_initial = link.replace("https://www.costco.com/","")
-		for i in remove_initial:
-			if(i!="."):
-				description += i
-			else:
-				break
-		description  = description.replace('-',' ')
-		print(f'Extracted item/search_term/description to be searched:\n {description}')
-	except:
-		print("Can't pull the description from costco url.")
-		description = ''
-	print('-'*10)	
-	return description
+    """
+
+    :param link:
+    :return:
+    """
+    description = ""
+    try:
+        remove_initial = link.replace("https://www.costco.com/", "")
+        for i in remove_initial:
+            if i != ".":
+                description += i
+            else:
+                break
+        description = description.replace("-", " ")
+        print(
+            f"Extracted item/search_term/description to be searched:\n >>{description}<<"
+        )
+    except:
+        print("Can't pull the description from costco url.")
+        description = ""
+    print("-" * 10)
+    return description
