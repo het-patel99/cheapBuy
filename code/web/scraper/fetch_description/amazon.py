b24f87f22ff572a77b3e6a2f40e5e283d70897f8
aakriti0fnu <aakriti.cse@gmail.com>
2021-11-01 20:04:06 -0400

    update amazon scraper.

--- code/web/scraper/fetch_description/amazon.py
+++ code/web/scraper/fetch_description/amazon.py
@@ -1,16 +1,23 @@
 def description_from_url_amazon(link):
-	description = ''
-	try:
-		link = link.replace('https://www.amazon.com/','')
-		for ch in link:
-			if ch!='/':
-				description+=ch
-			else:
-				break
-		description = description.replace('-', ' ')
-		print(f'Extracted item/search_term/description to be searched:\n {description}')
-	except:
-		print("Can't pull the description from amazon url.")
-		description = ''
-	print('-'*10)	
-	return description
+    """
+
+    :param link:
+    :return:
+    """
+    description = ""
+    try:
+        link = link.replace("https://www.amazon.com/", "")
+        for ch in link:
+            if ch != "/":
+                description += ch
+            else:
+                break
+        description = description.replace("-", " ")
+        print(
+            f"Extracted item/search_term/description to be searched: \n >>{description}<<"
+        )
+    except:
+        print("Can't pull the description from amazon url.")
+        description = ""
+    print("-" * 10)
+    return description
