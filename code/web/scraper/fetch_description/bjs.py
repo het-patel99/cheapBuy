e4772e5cd4748ebe1ac255eac2f34dca48cd9271
aakriti0fnu <aakriti.cse@gmail.com>
2021-11-01 20:04:34 -0400

    update bjs scraper.

--- code/web/scraper/fetch_description/bjs.py
+++ code/web/scraper/fetch_description/bjs.py
@@ -1,17 +1,24 @@
 def description_from_url_bjs(link):
-    description = ''
+    """
+
+    :param link:
+    :return:
+    """
+    description = ""
     try:
-        remove_initial = link.replace("https://www.bjs.com/product/","")
+        remove_initial = link.replace("https://www.bjs.com/product/", "")
         print(remove_initial)
         for i in remove_initial:
-            if(i!="/"):
+            if i != "/":
                 description += i
             else:
                 break
-        description  = description.replace('-',' ')
-        print(f'Extracted item/search_term/description to be searched:\n {description}')
+        description = description.replace("-", " ")
+        print(
+            f"Extracted item/search_term/description to be searched:\n >>{description}<<"
+        )
     except:
         print("Can't pull the description from Bjs url.")
-        description=''
-    print('-'*10)	    
+        description = ""
+    print("-" * 10)
     return description
