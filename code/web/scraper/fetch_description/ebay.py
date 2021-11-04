bc4b5a1b4e8037dc058e0aebc54606c5b23bd3b2
Sharath Kumar <sharu.bhavimane@gmail.com>
2021-11-04 04:10:08 -0400

    Fixed the scrappers for amazon and ebay. Added a new method find_best_matching_product to get relevant product instead of hardcoding to first search result. Removed driver code to improve the execution speed

--- code/web/scraper/fetch_description/ebay.py
+++ code/web/scraper/fetch_description/ebay.py
@@ -1,27 +1,39 @@
-from bs4 import BeautifulSoup
+from collections import namedtuple
 import requests
+from typing import NamedTuple
+from bs4 import BeautifulSoup
 
 
 def description_from_url_ebay(link):
-
     """
+    Parameters
+    ----------
+    link : str
+        website url
 
-    :param link:
-    :return:
+    Returns
+    -------
+    description: NamedTuple
+        NamedTuple named Description, contains product title and price
     """
     try:
-        headers = {
-            "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36"
-        }
+        headers = {"user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, "
+                                 "like Gecko) Chrome/90.0.4430.93 Safari/537.36 "}
         html = requests.get(link, headers=headers)
-        soup = BeautifulSoup(html.content, "html.parser")
-        product = soup.find(
-            "div", {"class": "vi-swc-lsp"}, {"id": "itemTitle"})
-        title = product.find("span", class_="u-dspn").text.strip()
-        print(f"Extracted item/search_term/title to be searched:\n {title}")
-    except:
-        print("Can't pull the description from Ebay url.")
-        title = ""
-    print("-" * 10)
+        if html.status_code == 200:
+            soup = BeautifulSoup(html.content, "html.parser")
+            # print(soup.prettify())
+            product = soup.find("div", {"class": "vi-swc-lsp"}, {"id": "itemTitle"})
+            title = product.find("span", class_="u-dspn").text.strip()
+            print(F"Extracted title: {title}")
+            price_ = soup.find("div", {"class": "u-flL w29 vi-price"}, {"id": "vi-mskumap-none"})
+            price = price_.find("span", class_="notranslate").text.strip('US $')
+            print(F"Extracted price: {price}")
+            description = namedtuple("Description", "title price")
+            return description(title, price)
+        else:
+            raise ConnectionRefusedError("Website does not allow scraping")
+    except Exception as e:
+        print(e)
+        return None
 
-    return title
